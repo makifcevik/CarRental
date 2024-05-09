dafd73dd7811e118337a177c0222e1dd3c1f390e
makifcevik <makif.cevik02@gmail.com>
2024-05-09 14:06:46 +0300

    Implemented Entity Framework

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,27 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car == null) throw new ArgumentNullException();
+            if(car.Name.Length >= 2 && car.DailyPrice > 0)
+                _carDal.Add(car);
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetAllByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetAllByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
         }
     }
 }
