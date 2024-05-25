c241388f556290c900ebcf881dc9e7f997eabd3f
makifcevik <makif.cevik02@gmail.com>
2024-05-25 16:12:00 +0300

    CarDetailDto updated to include images, default image added for cars without images

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -16,17 +16,23 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public List<CarDetailDto> GetAllCarDetails()
         {
+
             using(CarRentalContext context = new CarRentalContext())
             {
                 var result = from car in context.Cars
                              join brand in context.Brands on car.BrandId equals brand.Id
                              join color in context.Colors on car.ColorId equals color.Id
+                             join image in context.CarImages on car.Id equals image.CarId into carImages
                              select new CarDetailDto
                              {
                                  CarName = car.Name,
                                  BrandName = brand.Name,
                                  ColorName = color.Name,
                                  DailyPrice = car.DailyPrice,
+                                 Images = carImages.Any() 
+                                    ? carImages.Select(img => img.ImageName).ToList() 
+                                    : new List<string> { "default.jpg" }
+                                 
                              };
                 return result.ToList();
             }
