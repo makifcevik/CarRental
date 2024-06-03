1cd244195cff94783ebd002e733064c5fa9ddacc
makifcevik <makif.cevik02@gmail.com>
2024-06-03 13:22:14 +0300

    ClaimExtensions, ClaimsPrincipalExtensions added, JwtHelper implemented.

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,5 +1,8 @@
 ﻿using Core.Entities.Concrete;
+using Core.Extensions;
+using Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -23,15 +26,39 @@ namespace Core.Utilities.Security.Jwt
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+            var jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+            return new AccessToken
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
         }
-        public JwtSecurityToken CreateJwtSecurityToken()
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user,
+            SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var jwt = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                signingCredentials: signingCredentials,
+                notBefore: DateTime.Now,
+                claims: SetClaims(user, operationClaims)
+            );
+            return jwt;
         }
-        private IEnumerable<Claim> SetClaims()
+        private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>();
+            claims.AddEmail(user.Email);
+            claims.AddName($"{user.FirstName} {user.LastName}");
+            claims.AddNameIdentifier(user.Id.ToString());
+            claims.AddRoles(operationClaims.Select(x => x.Name).ToArray());
+            return claims;
         }
     }
 }
